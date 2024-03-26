5d91ba6dc19f0e0eed93b0327a3bc007026aa168
Aimar Abimayu Pratama <aimarabimanyu123@gmail.com>
2024-03-26 21:59:38 +0700

    beautify the code

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 
+
 def main():
     # Load the environment variable
     load_dotenv()
@@ -26,7 +27,8 @@ def main():
         embed = discord.Embed()
         embed.set_image(url="https://i.imgur.com/Le2xPHN.png")
 
-        await channel.send(f"Selamat datang {member.mention}, jangan lupa untuk nyalakan notifikasi pada <#{os.getenv('GARAPAN_CHANNEL_ID')}> untuk dapat update garapan baru dengan melakukan \n\n"
+        await channel.send(f"Selamat datang {member.mention}, jangan lupa untuk nyalakan notifikasi pada "
+                           f"<#{os.getenv('GARAPAN_CHANNEL_ID')}> untuk dapat update garapan baru dengan melakukan \n\n"
                            f"> Klik kanan di <#{os.getenv('GARAPAN_CHANNEL_ID')}> "
                            f"> Kik `Notification Setting` "
                            f"> Centang `New Posts Created` \n\n"
@@ -38,22 +40,25 @@ def main():
         if message.author == client.user:
             return
 
-        print(message.raw_role_mentions)
-
         try:
             if isinstance(message.channel.parent, discord.channel.ForumChannel):
-                if message.raw_role_mentions == [int(os.getenv('UPDATE_GARAPAN_ROLE_ID'))] or all(role in message.raw_role_mentions for role in [int(os.getenv('UPDATE_GARAPAN_ROLE_ID')), int(os.getenv('NAKAMA_ROLE_ID'))]):
+                if (message.raw_role_mentions == [int(os.getenv('UPDATE_GARAPAN_ROLE_ID'))]
+                    or all(role in message.raw_role_mentions for role in
+                           [int(os.getenv('UPDATE_GARAPAN_ROLE_ID')), int(os.getenv('NAKAMA_ROLE_ID'))])):
                     channel = client.get_channel(int(os.getenv('PING_GARAPAN_CHANNEL_ID')))
                     msg_url = message.jump_url
                     message_date = message.created_at
 
-                    embed = discord.Embed(title=f"{msg_url}", description=f"{message.content}", color=discord.Color.yellow())
+                    embed = discord.Embed(title=f"{msg_url}",
+                                          description=f"{message.content}",
+                                          color=discord.Color.yellow())
 
                     embed.set_author(name=message.author.global_name, icon_url=message.author.avatar)
                     if message.attachments:
                         embed.set_image(url=message.attachments[0].url)
                     embed.set_footer(text=f'Message sent at {message_date.strftime("%d %b %Y %H:%M")}')
-                    await channel.send(f"<@&{int(os.getenv('NAKAMA_ROLE_ID'))}> ada update baru di {message.channel.name}", embed=embed)
+                    await channel.send(f"<@&{int(os.getenv('NAKAMA_ROLE_ID'))}> "
+                                       f"ada update baru di {message.channel.name}", embed=embed)
         except:
             print('Pesan tidak terdeteksi parent forum channel')
 
