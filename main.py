1e73ca6ffda05fc78af245d704956a32bfa2c53f
Aimar Abimayu Pratama <aimarabimanyu123@gmail.com>
2024-04-02 06:28:31 +0700

    add edit message feature to feed notification

--- main.py
+++ main.py
@@ -23,16 +23,16 @@ def main():
     # Display welcome message when a new member join the server
     @client.event
     async def on_member_join(member):
-        channel = client.get_channel(int(os.getenv('WELCOME_CHANNEL_ID')))
+        welcome_channel = client.get_channel(int(os.getenv('WELCOME_CHANNEL_ID')))
         embed = discord.Embed()
         embed.set_image(url="https://i.imgur.com/Le2xPHN.png")
 
-        await channel.send(f"Selamat datang {member.mention}, jangan lupa untuk nyalakan notifikasi pada "
-                           f"<#{os.getenv('GARAPAN_CHANNEL_ID')}> untuk dapat update garapan baru dengan melakukan \n\n"
-                           f"> Klik kanan di <#{os.getenv('GARAPAN_CHANNEL_ID')}> "
-                           f"> Kik `Notification Setting` "
-                           f"> Centang `New Posts Created` \n\n"
-                           f"Contoh: ", embed=embed)
+        await welcome_channel.send(f"Selamat datang {member.mention}, jangan lupa untuk nyalakan notifikasi pada "
+                                   f"<#{os.getenv('GARAPAN_CHANNEL_ID')}> untuk dapat update garapan baru dengan melakukan \n\n"
+                                   f"> Klik kanan di <#{os.getenv('GARAPAN_CHANNEL_ID')}> "
+                                   f"> Kik `Notification Setting` "
+                                   f"> Centang `New Posts Created` \n\n"
+                                   f"Contoh: ", embed=embed)
 
     # Display feed message on #ping-post-garapan when roles get mentioned in message at #diskusi-garapan thread channel
     @client.event
@@ -43,7 +43,7 @@ def main():
         try:
             if isinstance(message.channel.parent, discord.channel.ForumChannel):
                 if message.raw_role_mentions == [int(os.getenv('UPDATE_GARAPAN_ROLE_ID'))] or all(role in message.raw_role_mentions for role in [int(os.getenv('UPDATE_GARAPAN_ROLE_ID')), int(os.getenv('NAKAMA_ROLE_ID'))]):
-                    channel = client.get_channel(int(os.getenv('PING_GARAPAN_CHANNEL_ID')))
+                    ping_garapan_channel = client.get_channel(int(os.getenv('PING_GARAPAN_CHANNEL_ID')))
                     msg_url = message.jump_url
 
                     embed = discord.Embed(title=f"{msg_url}",
@@ -54,8 +54,8 @@ def main():
                         embed.set_image(url=message.attachments[0].url)
                     embed.set_footer(text=f'{message.id}')
 
-                    await channel.send(f"<@&{int(os.getenv('NAKAMA_ROLE_ID'))}> "
-                                       f"ada update baru di {message.channel.name}", embed=embed)
+                    await ping_garapan_channel.send(f"<@&{int(os.getenv('NAKAMA_ROLE_ID'))}> "
+                                                    f"ada update baru di {message.channel.name}", embed=embed)
         except:
             print('Pesan tidak terdeteksi parent forum channel')
 
@@ -65,22 +65,41 @@ def main():
         if before.author == client.user:
             return
 
+        ping_garapan_channel = client.get_channel(int(os.getenv('PING_GARAPAN_CHANNEL_ID')))
+        msg_url = after.jump_url
+
         try:
             if isinstance(after.channel.parent, discord.channel.ForumChannel):
-                if (before.edited_at is None and after.edited_at is not None):
-                    if (after.raw_role_mentions == [int(os.getenv('UPDATE_GARAPAN_ROLE_ID'))] or all(role in after.raw_role_mentions for role in [int(os.getenv('UPDATE_GARAPAN_ROLE_ID')), int(os.getenv('NAKAMA_ROLE_ID'))])):
-                        channel = client.get_channel(int(os.getenv('PING_GARAPAN_CHANNEL_ID')))
-                        msg_url = after.jump_url
+                async for message in ping_garapan_channel.history(limit=500):
+                    if message.embeds[0].footer.text != str(before.id):
+                        if after.raw_role_mentions == [int(os.getenv('UPDATE_GARAPAN_ROLE_ID'))] or all(role in after.raw_role_mentions for role in [int(os.getenv('UPDATE_GARAPAN_ROLE_ID')), int(os.getenv('NAKAMA_ROLE_ID'))]):
+                            embed = discord.Embed(title=f"{msg_url}",
+                                                  description=f"{after.content}",
+                                                  color=discord.Color.yellow())
+                            embed.set_author(name=after.author.global_name, icon_url=after.author.avatar)
+                            if after.attachments:
+                                embed.set_image(url=after.attachments[0].url)
+                            embed.set_footer(text=f'{after.id}')
 
-                        embed = discord.Embed(title=f"{msg_url}",
-                                              description=f"{after.content}",
-                                              color=discord.Color.yellow())
-                        embed.set_author(name=after.author.global_name, icon_url=after.author.avatar)
-                        if after.attachments:
-                            embed.set_image(url=after.attachments[0].url)
-                        embed.set_footer(text=f'{after.id}')
-                        await channel.send(f"<@&{int(os.getenv('NAKAMA_ROLE_ID'))}> "
-                                           f"ada update baru di {after.channel.name}", embed=embed)
+                            await ping_garapan_channel.send(f"<@&{int(os.getenv('NAKAMA_ROLE_ID'))}> "
+                                                            f"ada update baru di {after.channel.name}", embed=embed)
+
+                            break
+                    elif message.embeds[0].footer.text == str(before.id):
+                        if after.raw_role_mentions == [int(os.getenv('UPDATE_GARAPAN_ROLE_ID'))] or all(role in after.raw_role_mentions for role in [int(os.getenv('UPDATE_GARAPAN_ROLE_ID')), int(os.getenv('NAKAMA_ROLE_ID'))]):
+                            embed = discord.Embed(title=f"{msg_url}",
+                                                  description=f"{after.content}",
+                                                  color=discord.Color.yellow())
+                            embed.set_author(name=after.author.global_name, icon_url=after.author.avatar)
+                            if after.attachments:
+                                embed.set_image(url=after.attachments[0].url)
+                            embed.set_footer(text=f'{after.id}')
+
+                            await message.delete()
+                            await ping_garapan_channel.send(f"<@&{int(os.getenv('NAKAMA_ROLE_ID'))}> "
+                                                            f"ada update baru di {after.channel.name}", embed=embed)
+
+                            break
         except:
             print('Pesan tidak terdeteksi parent forum channel')
 
